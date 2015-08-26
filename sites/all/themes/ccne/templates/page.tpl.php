8581859e94966c98a83ab1cc82ab3f4f0af05e47
pradeep kumar <pradeep.kumar@ebizontek.com>
2015-08-26 19:10:39 +0530

    changes in menu

--- sites/all/themes/ccne/templates/page.tpl.php
+++ sites/all/themes/ccne/templates/page.tpl.php
@@ -184,7 +184,7 @@
 			</div>
 	       <?php endif; ?>
 
-                <?php /*if ($main_menu): ?>
+                <?php if ($main_menu): ?>
                   <div id="main-menu" class="navigation">
                       <?php
                       print theme('links__system_main_menu', array(
@@ -198,17 +198,9 @@
                           'level' => 'h2',
                           'class' => array('element-invisible'),
                         ),
-                      ));*/
+                      ));
                       ?>
-                  <!--/div> <!-- /#main-menu -->
-                  
-		  <?php if ($page['header_menu']): ?>
-			<div id="main-menu" class="navigation">
-			    <?php print render($page['header_menu']); ?>
-			</div>
-	       <?php endif; ?>
-                  
-                  
+                  </div> <!-- /#main-menu -->
                 <?php endif; ?>
                 <?php /* if ($breadcrumb): ?>
                   <div id="breadcrumb"><?php print $breadcrumb; ?></div>
